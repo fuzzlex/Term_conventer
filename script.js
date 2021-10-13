3645fb2d309ec5d6942a9eaa1658285a251fcb8c
emre <emrky.2018@icloud.com>
2021-10-13 21:23:48 +0300

    update

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
 
-const celTofahrenheit = (cel) => cel * 1.8 + 32;
-const fahTocelcius = (fah) => (fah - 32) * 5 / 9;
-const celTokelvin = (cel) => cel + 273.15 ;
-const fahTokelvin = (fah) => (5 * (fah - 32) / 9) + 273;
+
+let textContainerEl = document.getElementById("textContainer");
+let numEl = document.getElementById("num");
+let openedNewEl = document.getElementById("openedNew");
+let resultEl = document.getElementById("resultE")
+
 
 
 function tempConventer() {
     let form = document.getElementsByName("converter")
     form.forEach(radio => {
-    if (radio.checked){
-        checkedR = (radio.value);
-    }
+    if (radio.checked){checkedR = (radio.value);}
 })
-
-  while (true){
-  if (checkedR == "celtofah"){
-      console.log(celTofahrenheit(20));
-      break;}
-  else if (checkedR == "fahtocel"){
-          console.log(fahTocelcius(20));
-          break;}
-  else if (checkedR == "celtokel"){
-          console.log(celTokelvin(20));
-          break;}
-  else if (checkedR == "fahtokel"){
-          console.log(fahTokelvin(20));
-          break;}
-  }   
+    textContainerEl.style.display = "none";
+    openedNewEl.style.display = "inline-block";
+    
 }
+
+function enterNum(){
+
+    const celTofahrenheit = (cel) => cel * 1.8 + 32;
+    const fahTocelcius = (fah) => (fah - 32) * 5 / 9;
+    const celToKelvin = (cel) => (cel + 273) ;
+    const fahTokelvin = (fah) => (5 * (fah - 32) / 9) + 273;
+
+    let numIn = numEl.value;
+
+    if (checkedR == "celtofah"){
+        resultEl.innerHTML = numIn + " Celcius = " + celTofahrenheit(numIn).toFixed(2) + "  Fahrenheit";
+        }
+    else if (checkedR == "fahtocel"){
+        resultEl.innerHTML =  numIn + " Fahrenheit = " + fahTocelcius(numIn).toFixed(2) + "  Celcius";
+            }
+    else if (checkedR == "celtokel"){
+        resultEl.innerHTML = numIn + " Celcius = " + celToKelvin(numIn) + "  Kelvin";
+            }
+    else if (checkedR == "fahtokel"){
+        resultEl.innerHTML = numIn + " Fahrenheit = " + fahTokelvin(numIn).toFixed(2) + "  Kelvin";
+            }
+}
+  
+
